093842ac3c1212b0bf2296ebee74c6cf7b1ae1aa
unknown <orotem@7SSTWZ1.agoda.local>
2015-05-11 10:37:13 +0700

    l\ \

--- src/datacreator/runner.scala
+++ src/datacreator/runner.scala
@@ -7,10 +7,13 @@ package datacreator
 object runner extends App{
 	override def main(args: Array[String]) {
 		val dist = List(("A",1.0),("B",2.0), ("C",1.0))
-		val rvg : RandomValueGenerator[String] = new RandomValueGenerator(dist) with SerialUniformGenerator
+		val rvg : RandomValueGenerator[String] = new RandomValueGenerator(dist) //with SerialUniformGenerator
 
 		//val res = ((1 to 1000).map((i: Int) => rvg.getNext())).toList.groupBy(identity).mapValues(_.size)
 		//println(res)
+		for (i <- 1 to 10000) {
+			println(rvg.getNext())
+		}
 		println("x")
 	}
 }
